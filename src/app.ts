4d16172443685a078969af70df1e43933e4b9e0a
Sohan1928 <sohanurrahman1928@gmail.com>
2024-12-18 22:29:48 +0600

    assignment 3 project all setup complete

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// // application routes
+// app.use("/api/v1", router);
+
+const test = async (req: Request, res: Response) => {
+  const a = "Hello Developer!!";
+  res.send(a);
+};
+
+app.get("/", test);
+
+// app.use(globalErrorHandler);
+
+// //Not Found
+// app.use(notFound);
+
+export default app;
